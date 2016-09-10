ed7e6bdc2757588fb899648924bf826305ded2fe
dukeimg <duke.img@gmail.com>
2016-09-10 19:56:05 +0300

    Production ACtionCable allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = ['*servik.herokuapp.com/*']
+  config.web_socket_server_url = 'wss://servik.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://servik.herokuapp.com', 'http://servik.herokuapp.com']
+
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
