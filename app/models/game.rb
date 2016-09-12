e423080f1473bcd11b44ca2a83be1bb6b6e771d9
dukeimg <duke.img@gmail.com>
2016-09-12 18:41:56 +0300

    Sub msg

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,8 @@ class Game
   def self.start(uuid1, uuid2)
     first, second = [uuid1, uuid2].shuffle
 
-    ActionCable.server.broadcast "player_#{first}", {action: "game_start", msg: "first"}
-    ActionCable.server.broadcast "player_#{second}", {action: "game_start", msg: "second"}
+    # ActionCable.server.broadcast "player_#{first}", {action: "game_start", msg: "first"}
+    # ActionCable.server.broadcast "player_#{second}", {action: "game_start", msg: "second"}
 
     REDIS.set("opponent_for:#{first}", second)
     REDIS.set("opponent_for:#{second}", first)
@@ -23,6 +23,6 @@ class Game
     opponent = opponent_for(uuid)
     turn_string = "#{data["from"]}-#{data["to"]}"
 
-    ActionCable.server.broadcast "player_#{opponent}", {action: "turn", msg: turn_string}
+    # ActionCable.server.broadcast "player_#{opponent}", {action: "turn", msg: turn_string}
   end
 end
