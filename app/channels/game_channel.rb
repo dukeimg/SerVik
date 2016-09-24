3f5bf44b1de7d0e69bdb288b6bb5fd24ee967597
dukeimg <duke.img@gmail.com>
2016-09-24 19:45:46 +0300

    Вывод количества игроков онлайн

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -2,11 +2,13 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
     stream_from "player_#{uuid}"
+    ActionCable.server.broadcast('game', ActionCable.server.connections.size)
   end
 
   def unsubscribed
     Seek.remove(uuid)
     Game.opponent_disconnected(uuid)
+    ActionCable.server.broadcast('game', ActionCable.server.connections.size)
   end
 
   def set_code(data)
