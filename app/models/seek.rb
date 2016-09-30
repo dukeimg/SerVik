3ca759ea3c2f967a4d728040dc64cd029e6207d9
dukeimg <duke.img@gmail.com>
2016-09-30 17:33:52 +0300

    Попытка исправить /app/app/models/seek.rb:46: warning: else without rescue is useless

--- app/models/seek.rb
+++ app/models/seek.rb
@@ -30,7 +30,8 @@ class Seek
 
   def init_seek_with_filters(uuid, data)
     filter = data['filter']
-    active_filters = filter.select {|key, value| value if data[key] != 0} if data['filter'].exists? else ''
+    # active_filters = filter.select {|key, value| value if data[key] != 0} if data['filter'].exists? else ''
+    active_filters = filter.select {|key, value| value if data[key] != 0} || ''
 
     # Temporal debug messages
     puts "data: #{data}"
