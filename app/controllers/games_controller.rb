b7144e141755f9e9fc3ff91215aebcbb611bf6cd
dukeimg <duke.img@gmail.com>
2016-10-02 00:30:12 +0300

    Поменял название

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def welcome
     respond_to do |format|
-      format.html { render 'games/welcome'}
+      format.html {render 'games/welcome'}
     end
   end
 end
