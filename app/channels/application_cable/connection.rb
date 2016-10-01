67a0126d465118d0c3647be9cd3b68ab3862b8c9
dukeimg <duke.img@gmail.com>
2016-10-01 11:28:11 +0300

    Дебаггерская хня

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -6,18 +6,12 @@ module ApplicationCable
       self.uuid = SecureRandom.uuid
 
       REDIS.sadd('players_online', uuid)
-      players_online = REDIS.get('players_online').size
-      players_online.each do |player|
-        ActionCable.server.broadcast player, {title: 'players_online', message: players_online}
-      end
+      notify_players
     end
 
     def disconnect
-      players_online = REDIS.get('players_online').to_i
-      players_online -= 1
-      REDIS.set('players_online', players_online)
-      transmit({'title': 'players_online', 'message': players_online})
-      puts "Игроков в сети #{players_online}" # debug
+      REDIS.srem('players_online', uuid)
+      notify_players
 
       if Game.opponent_for(self.uuid)
         Game.opponent_disconnected(self.uuid)
@@ -27,6 +21,14 @@ module ApplicationCable
     def receive(websocket_message)
       send_async :dispatch_websocket_message, websocket_message
       puts "!!!DEBUG #{websocket_message}"
+    end
+
+    def notify_players
+      players_online = REDIS.get('players_online')
+      puts "Игроков в сети #{players_online.size}"
+      players_online.each do |player|
+        ActionCable.server.broadcast player, {title: 'players_online', message: players_online.size}
+      end
     end
   end
 end
