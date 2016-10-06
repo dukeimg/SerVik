acaf092231f659b48449abbb4c8eabb7db085679
dukeimg <duke.img@gmail.com>
2016-10-06 21:32:35 +0300

    create_mock_room helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,6 +1,7 @@
 module GamesHelper
   def create_mock_room
     filter = {"mode"=>"tb", "time_limit"=>0, "turn_limit"=>0, "turn_time_limit"=>0, "title_game"=>"dummy"}
+    REDIS.hdel('seeks', 'fake_uuid')
     REDIS.hset("seeks", 'fake_uuid', filter)
   end
 end
