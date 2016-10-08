94dc89555966976ed855ec5fcf7b26beb9008a63
dukeimg <duke.img@gmail.com>
2016-10-08 16:22:58 +0300

    Улучшил create_mock_room

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,20 @@
 module GamesHelper
   def create_mock_room
-    filter = {"mode"=>"tb", "time_limit"=>0, "turn_limit"=>0, "turn_time_limit"=>0, "title_game"=>"dummy"}
-    REDIS.hdel('seeks', 'fake_uuid')
-    REDIS.hset("seeks", 'fake_uuid', filter)
+    10.times do |n|
+      f_1 = ['tb', 'rt'].shuffle.pop
+      f_2 = [3, 5, 10].shuffle.pop
+      f_3 = [5, 10, 20].shuffle.pop
+      f_4 = [15, 30, 60].shuffle.pop
+      name = ['Сергей', 'Витя', 'Дима', 'Андрей', 'Алина', 'Клара Ватсон', 'Таня', 'Шишка', 'Ляшка'].shuffle.pop
+      filter = {"mode"=>f_1, "time_limit"=>f_2, "turn_limit"=>f_3, "turn_time_limit"=>f_4, "title_game"=>name}
+      REDIS.hdel('seeks', "fake_uuid_#{n}")
+      REDIS.hset("seeks", "fake_uuid_#{n}", filter)
+    end
   end
 
   def send_rooms_data(msg)
     ActionCable.server.connections.each do |connection|
-      connection.transmit({"identifier":"{\"channel\":\"GameChannel\"}","message":msg})
+      connection.transmit({"identifier":"{\"channel\":\"GameChannel\"}", "message":msg})
     end
   end
 end
