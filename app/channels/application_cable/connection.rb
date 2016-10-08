f122dff50095f0c602d13180e6e6e23c36aeab00
dukeimg <duke.img@gmail.com>
2016-10-08 21:02:41 +0300

    Исправил баг отображения игроков онлайн в браузере.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,14 +10,21 @@ module ApplicationCable
       msg = {"identifier":"{\"channel\":\"NotificationsChannel\"}","message":{"title":"players_online","message":ActionCable.server.connections.size + 1}}
       transmit(msg)
       send_rooms_data(msg)
+      send_players_online(ActionCable.server.connections.size + 1)
     end
 
     def disconnect
       msg = {"identifier":"{\"channel\":\"NotificationsChannel\"}","message":{"title":"players_online","message":ActionCable.server.connections.size}}
       send_rooms_data(msg)
-
+      send_players_online(ActionCable.server.connections.size)
       if Game.opponent_for(self.uuid)
         Game.opponent_disconnected(self.uuid)
+      end
+    end
+
+    def send_players_online(msg)
+      ActionCable.server.connections.each do |connection|
+        connection.transmit({"identifier":"{\"channel\":\"NotificationsChannel\"}","message":{"title":"players_online","message":msg}})
       end
     end
 
