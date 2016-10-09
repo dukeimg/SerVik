114516d9b287e7f69b31142e85e679a8c4fd6438
dukeimg <duke.img@gmail.com>
2016-10-09 20:52:21 +0300

    Включил защиту

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.disable_request_forgery_protection = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
