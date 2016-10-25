1f78013e7a1ea2ab764076649a6ae28bf6b04546
dukeimg <duke.img@gmail.com>
2016-10-25 18:17:20 +0300

    Жесть

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -12,6 +12,19 @@ module GamesHelper
     end
   end
 
+  def create_mock_room_9000
+    9000.times do |n|
+      f_1 = ['tb', 'rt'].shuffle.pop
+      f_2 = [3, 5, 10].shuffle.pop
+      f_3 = [5, 10, 20].shuffle.pop
+      f_4 = [15, 30, 60].shuffle.pop
+      name = ['Сергей', 'Витя', 'Дима', 'Андрей', 'Алина', 'Клара Ватсон', 'Таня', 'Шишка', 'Ляшка'].shuffle.pop
+      filter = {"mode"=>f_1, "time_limit"=>f_2, "turn_limit"=>f_3, "turn_time_limit"=>f_4, "title_game"=>name}
+      REDIS.hdel('seeks', "fake_uuid_#{n}")
+      REDIS.hset("seeks", "fake_uuid_#{n}", filter)
+    end
+  end
+
   def send_rooms_data(msg)
     ActionCable.server.connections.each do |connection|
       connection.transmit({"identifier":"{\"channel\":\"GameChannel\"}", "message":msg})
